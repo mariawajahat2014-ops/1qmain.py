0e80c5291aee18ff548375ee6315234bff920806
mariawajahat2014-ops <mariawajahat2014@gmail.com>
2026-03-15 13:29:12 +0500

    Add files via upload

---
+++ 1qmain.py
@@ -0,0 +1,24 @@
+a = 11
+b = 13
+c = 0
+
+if a and b and c:
+    print("ALL the numbers has boolean value as True")
+else:
+    print("At least one number has boolean value as true")
+
+
+
+a = 10
+b = -10
+c = 0
+
+if a > 0 or b > 0:
+    print("Either of the number is greater than 0")
+else:
+    print("NO number is greater than 0") 
+
+if b > 0 or c > 0:
+    print("Either of the number is greater than 0")   
+else:
+    print("NO number is greater than 0")        
